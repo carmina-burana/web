7ba84f9b3e7cf35307f97e7bec08553f3ce1a552
Kesha <affenmilchmann@gmail.com>
2023-02-26 19:58:05 +0300

    крестик, ссылки, шрифт

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 function openNav() {
     document.getElementById("rightSidenav").style.width = "300px";
-    var btn = document.getElementById("navbtn");
-    btn.style.opacity = 0;
-    btn.style.visibility = "hidden";
+    var nav_open_btn = document.getElementById("nav_open_btn");
+    var nav_close_btn = document.getElementById("nav_close_btn");
+    nav_open_btn.style.visibility = "hidden";
+    nav_close_btn.style.visibility = "visible";
 }
 function closeNav() {
     document.getElementById("rightSidenav").style.width = "0";
-    var btn = document.getElementById("navbtn");
-    btn.style.opacity = 1;
-    btn.style.visibility = "visible";
+    var nav_open_btn = document.getElementById("nav_open_btn");
+    var nav_close_btn = document.getElementById("nav_close_btn");
+    nav_open_btn.style.visibility = "visible";
+    nav_close_btn.style.visibility = "hidden";
 }
 function toggleList(id) {
     var elem = document.getElementById(id);
