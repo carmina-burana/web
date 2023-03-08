35e1f62c6d9439dd8da9cc77efaa65af2e1d1fd9
Kesha <none>
2023-03-08 17:24:14 +0300

    working

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
 function openNav() {
-    document.getElementById("rightSidenav").style.width = "300px";
+    document.getElementById("rightSidenav").style.width = "250px";
     var nav_open_btn = document.getElementById("nav_open_btn");
     var nav_close_btn = document.getElementById("nav_close_btn");
+    var nav_header = document.getElementById("nav_header");
+    var nav_body = document.getElementById("side_nav_item");
+    
+    nav_header.style.float = "right";
+    nav_body.style.float = "right";
+    nav_header.style.visibility = "visible";
+    nav_header.style.width = "230px";
+    nav_body.style.width = "80%";
+
     nav_open_btn.style.visibility = "hidden";
     nav_close_btn.style.visibility = "visible";
 }
 function closeNav() {
     document.getElementById("rightSidenav").style.width = "0";
     var nav_open_btn = document.getElementById("nav_open_btn");
-    var nav_close_btn = document.getElementById("nav_close_btn");
+    var nav_header = document.getElementById("nav_header");
+    var nav_body = document.getElementById("side_nav_item");
+    
+    nav_header.style.float = "none";
+    nav_body.style.float = "none";
+    nav_header.style.visibility = "hidden";
+    nav_header.style.width = "0px";
+    nav_body.style.width = "0px";
+
     nav_open_btn.style.visibility = "visible";
     nav_close_btn.style.visibility = "hidden";
 }
